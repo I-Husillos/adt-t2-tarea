bf15c334173752f33d8396e5c32ca4bf3183e016
I-Husillos <ivanhuslabra@gmail.com>
2024-11-04 21:22:05 +0100

    sii

--- src/main/java/hr/management/tools/JsonManager.java
+++ src/main/java/hr/management/tools/JsonManager.java
@@ -35,7 +35,6 @@ public class JsonManager {
         }
     }
 
-
     public static boolean saveVehiclesToJson(ArrayList<Vehicle> vehicles, String ruta){
         try {
             objectMapper.writeValue(new File(ruta), vehicles);
